3532d3b7ede6749fe894dd8d016ff94c61cc0df6
fernanAF <alan.fdo17@gmail.com>
2023-12-01 19:22:36 -0600

    Configuracion de TimeOuts Hystrix

---
+++ springboot-service-store/src/main/java/com/microservice/store/models/Celular.java
@@ -0,0 +1,65 @@
+package com.microservice.store.models;
+
+import java.util.Date;
+
+public class Celular {
+	
+	private Long id;
+	
+	private String marca;
+	
+	private String modelo;
+	
+	private Date createdAt;
+	
+	private Integer port;
+	
+	public Celular(Long id, String marca, String modelo) {
+		this.id = id;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
+	
+
+}
